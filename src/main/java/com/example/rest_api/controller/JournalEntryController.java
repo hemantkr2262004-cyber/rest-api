59d2922f916ee7cb74fa9438bb33b3992932e1a9
Hemant Kumar <hemantkr2262004@gmail.com>
2026-09-26 15:51:39 +0530

    new_rest-api_code

--- src/main/java/com/example/rest_api/controller/JournalEntryController.java
+++ src/main/java/com/example/rest_api/controller/JournalEntryController.java
@@ -1,20 +1,30 @@
 package com.example.rest_api.controller;
 
 import com.example.rest_api.controller.entity.JournalEntry;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
 
     private Map<Long, JournalEntry> journalEntries = new HashMap<>();
 
     @GetMapping
-   public List<JournalEntry> getAll(){
-    return new ArrayList<>(journalEntries.values());
-     }
-
+   public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+   @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
+    }
+@GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+     return  journalEntries.get(myId);
+    }
 }
--- src/main/java/com/example/rest_api/controller/entity/JournalEntry.java
+++ src/main/java/com/example/rest_api/controller/entity/JournalEntry.java
@@ -1,4 +1,22 @@
 package com.example.rest_api.controller.entity;
 
 public class JournalEntry {
+    private long id;
+
+    private String title;
+
+    private String content;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
